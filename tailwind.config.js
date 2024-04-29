46823588e0ead5f0c611be401eaa0dbbf186b926
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-29 15:53:17 +0300

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+    colors: {
+        primary: '#fff',
+        secondary: '#F8F8F8',
+        footer:"#69C800",
+      },
+    },
+  },
+  plugins: [],
+}
