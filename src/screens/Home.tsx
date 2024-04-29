d3b8650882fd2a9b63d29dbd725870181f07e930
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-29 15:55:41 +0300

    staring the home page

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import home from '../assets/home2.png';
+
+const Home = () => {
+  return (
+    <div>
+      <img src={home} alt='' className='min-h-screen'/>
+    </div>
+  )
+}
+
+export default Home
