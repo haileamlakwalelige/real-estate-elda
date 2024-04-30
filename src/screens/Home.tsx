fe9f3c4dffd56ae956d3ed1e6aa361eb0d8419c3
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-30 16:18:29 +0300

    update home page.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,33 @@
 // import React from 'react'
 import home from '../assets/home2.png';
+import Hero from '../components/home/Hero';
+import HomeAbout from '../components/home/HomeAbout';
+import HomeAkakki from '../components/home/HomeAkakki';
+import HomeCredential from '../components/home/HomeCredential';
+import HomeExplore from '../components/home/HomeExplore';
+import HomeForm from '../components/home/HomeForm';
+// import HomeHero from '../components/home/HomeHero';
+import HomeWhy from '../components/home/HomeWhy';
+import HorizontalLine from '../components/reusable/HorizontalLine';
 
 const Home = () => {
   return (
-    <div>
+    <div className='bg-primary text-black overflow-x-hidden'>
       <img src={home} alt='' className='min-h-screen'/>
+      {/* <HomeHero /> */}
+      <Hero />
+      <HorizontalLine />
+      <HomeExplore />
+      <HorizontalLine />
+      <HomeAbout />
+      <HorizontalLine />
+      <HomeWhy />
+      <HorizontalLine />
+      <HomeAkakki />
+      <HorizontalLine />
+      <HomeCredential />
+      <HorizontalLine />
+      <HomeForm />
     </div>
   )
 }
