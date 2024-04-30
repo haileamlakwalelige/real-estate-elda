cd1866c770f5f2bd7d2beb58078b9a7b2f402d70
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-30 16:17:33 +0300

    add horizontal line

---
+++ src/components/reusable/HorizontalLine.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const HorizontalLine = () => {
+  return (
+    <div className='border-t-[1px] bg-white border-gray-300 my-10 mx-2 sm:mx-4 md:mx-6'></div>
+  )
+}
+
+export default HorizontalLine
