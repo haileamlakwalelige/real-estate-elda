a4ddb444f45fc936767c8a6e0284e5deaa238b3d
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-30 16:16:15 +0300

    add home hero

---
+++ src/components/home/HomeHero.tsx
@@ -0,0 +1,34 @@
+// import React from 'react'
+import './hero.css';
+
+const HomeHero = () => {
+  return (
+    <div>
+        <div className="flex overflow-x-auto rounded-box overflow-auto">
+  <div className="carousel-item">
+    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Pizza" className="w-screen h-screen"/>
+  </div> 
+  <div className="carousel-item">
+    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Pizza" className="w-screen h-screen"/>
+  </div> 
+  <div className="carousel-item">
+    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Pizza" className="w-screen h-screen"/>
+  </div> 
+  <div className="carousel-item">
+    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Pizza" className="w-screen h-screen"/>
+  </div> 
+  <div className="carousel-item">
+    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Pizza" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Pizza" className="w-screen h-screen"/>
+  </div> 
+  <div className="carousel-item">
+    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Pizza" className="w-screen h-screen"/>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default HomeHero;
