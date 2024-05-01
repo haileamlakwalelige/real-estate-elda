c0db8408db8aa44db9ef14450f7d9f08fc77d4eb
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-01 20:53:58 +0300

    finalize the home pages

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,32 +1,45 @@
-// import React from 'react'
-import './hero.css';
+// import React from 'react';
+import home1 from '../../assets/home1.png';
+import home2 from '../../assets/home2.png';
+import home3 from '../../assets/home3.png';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider, { Settings as SliderSettings } from "react-slick";
+import Button from '../reusable/Button';
 
 const HomeHero = () => {
+
+  const settings: SliderSettings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 3000,
+    cssEase: "linear"
+  };
+
   return (
     <div>
-        <div className="flex overflow-x-auto rounded-box overflow-auto">
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Pizza" className="w-screen h-screen"/>
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Pizza" className="w-screen h-screen"/>
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Pizza" className="w-screen h-screen"/>
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Pizza" className="w-screen h-screen"/>
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Pizza" className="w-screen h-screen"/>
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Pizza" className="w-screen h-screen"/>
-  </div>
-</div>
+       <Slider {...settings} className='z-10 relative'>
+        <div>
+          <img src={home1} alt="" className="w-screen h-screen" />
+        </div>
+        <div>
+          <img src={home2} alt="" className="w-screen h-screen" />
+        </div>
+        <div>
+          <img src={home3} alt="" className="w-screen h-screen" />
+        </div>
+      </Slider>
+      <div className='-mt-96 z-30 absolute ml-20'>
+        <p className='text-white font-bold text-[50px] md:text-[60px] lg:text-[70px] py-4'>ELDANA Spring Homes</p>
+        <p className='text-white text-[26px] md:text-[30px] lg:text-[34px] mb-8'>Where Luxury Meets Comfort</p>
+        <div className='ml-10'>
+        <Button type='button' text='Contact Us' />
+        </div>
+      </div>
     </div>
   )
 }
