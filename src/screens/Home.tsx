d72474e9fa2de4a808936bd4db071ca15c009032
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-01 22:11:57 +0300

    update home page by adding section

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,23 @@
 // import React from 'react'
-import home from '../assets/home2.png';
-import Hero from '../components/home/Hero';
+// import Hero from '../components/home/Hero';
 import HomeAbout from '../components/home/HomeAbout';
 import HomeAkakki from '../components/home/HomeAkakki';
 import HomeCredential from '../components/home/HomeCredential';
 import HomeExplore from '../components/home/HomeExplore';
 import HomeForm from '../components/home/HomeForm';
+import HomeHero from '../components/home/HomeHero';
+import HomeProperty from '../components/home/HomeProperty';
 // import HomeHero from '../components/home/HomeHero';
 import HomeWhy from '../components/home/HomeWhy';
 import HorizontalLine from '../components/reusable/HorizontalLine';
 
 const Home = () => {
+
+
+
   return (
     <div className='bg-primary text-black overflow-x-hidden'>
-      <img src={home} alt='' className='min-h-screen'/>
-      {/* <HomeHero /> */}
-      <Hero />
+      <HomeHero />
       <HorizontalLine />
       <HomeExplore />
       <HorizontalLine />
@@ -26,6 +28,8 @@ const Home = () => {
       <HomeAkakki />
       <HorizontalLine />
       <HomeCredential />
+      <HorizontalLine />
+      <HomeProperty />
       <HorizontalLine />
       <HomeForm />
     </div>
