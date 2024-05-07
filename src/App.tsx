046a010d48a56abd8a9fb53d7a7f93f26b95dab9
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-07 15:58:10 +0300

    update App section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./screens/Home"
 import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
 import Copy from "./components/layout/Copy"
+import Properties from "./screens/Properties"
 
 const App = () => {
 
@@ -15,7 +16,8 @@ const App = () => {
       <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/properties" element={<Properties />} />
       </Routes>
       <Footer />
       <Copy />
