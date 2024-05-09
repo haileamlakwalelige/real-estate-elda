aaa18ef28ce9473b89044b7ccde5beb253cf6db4
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-09 15:53:23 +0300

    add contact page and import both contact form and contact phones

---
+++ src/screens/Contact.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import ContactForm from "../components/contact/ContactForm"
+import ContactPhones from "../components/contact/ContactPhones"
+
+const Contact = () => {
+  return (
+    <div className="bg-white pt-32">
+        <ContactForm />
+        <ContactPhones />
+    </div>
+  )
+}
+
+export default Contact
