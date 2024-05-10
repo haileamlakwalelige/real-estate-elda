f8a233c8027906dd299c2d2f3b969fcf78a04889
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-10 09:44:23 +0300

    update about story styling

--- src/components/about/AboutStory.tsx
+++ src/components/about/AboutStory.tsx
@@ -19,19 +19,19 @@ const AboutStory = () => {
        <div className='px-2 md:px-4 lg:px-10 xl:px-16 flex flex-col'>
       <div className='flex justify-between items-center gap-10 w-screen lg:pr-28'>
         <div>
-        <p className='text-normal text-[30px] md:text-[35px] lg:text-[40px] text-black uppercase'>Our Story</p>
+        <p className='text-normal text-[30px] md:text-[35px] lg:text-[40px] text-gray-800 uppercase'>Our Story</p>
         <div className='flex'>
         <div className='h-[2px] w-[70px] text-red-500 bg-red-500'></div>
         <div className='h-[2px] w-[70px] text-blue-500 bg-blue-500'></div>
         <div className='h-[2px] w-[70px] text-green-500 bg-green-500'></div>
         </div>
-        <div className='mt-20 text-black text-[20px] md:text-[22px] '>
+        <div className='mt-20 text-gray-800 text-[20px] md:text-[22px] '>
           <p>Share the journey of Eldana Spring Homes, including its founding <br/> principles, values, and mission. Highlight any milestones or <br/> achievements that demonstrate the company's growth and success.</p>
         </div>
         </div>
          <div className='mt-10 hidden xl:flex flex-col items-center justify-start'>
           <img src={vis} alt='' className='max-h-[300px]'/>
-          <p className='text-black lg:-mt-40 lg:ml-32 '>Outline the company's vision for the <br/> future, including goals for expansion<br/>, innovation, and continued excellence in <br/> the real estate industry.</p>
+          <p className='text-gray-800 lg:-mt-40 lg:ml-32 '>Outline the company's vision for the <br/> future, including goals for expansion<br/>, innovation, and continued excellence in <br/> the real estate industry.</p>
          </div>
         </div>
         <div className='grid grid-cols-1 lg:grid-cols-3 justify-between items-center gap-10 w-screen lg:pr-28'>
@@ -40,11 +40,11 @@ const AboutStory = () => {
         </div>
           <div className='mt-10 hidden xl:flex flex-col items-center justify-start'>
           <img src={val} alt='' className='max-h-[300px]'/>
-          <p className='text-black lg:-mt-40 lg:ml-32 '>Detail the core values that guide Eldana <br/> Spring Homes in all its endeavors, such <br/> as integrity, professionalism, customer <br/> satisfaction, and a commitment to <br/> quality.</p>
+          <p className='text-gray-800 lg:-mt-40 lg:ml-32 '>Detail the core values that guide Eldana <br/> Spring Homes in all its endeavors, such <br/> as integrity, professionalism, customer <br/> satisfaction, and a commitment to <br/> quality.</p>
          </div>
          <div className='mt-10 hidden xl:flex flex-col items-center justify-start'>
           <img src={mis} alt='' className='max-h-[300px]'/>
-          <p className='text-black lg:-mt-40 lg:ml-32 '>our mission is to redefine the real estate <br/> experience by providing unparalleled <br/> service, exceptional properties, and <br/> personalized solutions to meet the <br/> diverse needs of our clients.</p>
+          <p className='text-gray-800 lg:-mt-40 lg:ml-32 '>our mission is to redefine the real estate <br/> experience by providing unparalleled <br/> service, exceptional properties, and <br/> personalized solutions to meet the <br/> diverse needs of our clients.</p>
          </div>
         </div>
        </div>
