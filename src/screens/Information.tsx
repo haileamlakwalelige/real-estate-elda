68d600c3c6c4f052c726c076c3e944e9ae6cab93
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-10 09:48:27 +0300

    adding and finalizing information page

---
+++ src/screens/Information.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+import InfoHero from "../components/information/InfoHero"
+import InfoWhy from "../components/information/InfoWhy"
+import InfoKalit from "../components/information/InfoKalit"
+import InfoTarget from "../components/information/InfoTarget"
+
+const Information = () => {
+  return (
+    <div className="bg-white">
+        <InfoHero />
+        <InfoWhy />
+        <InfoKalit />
+        <InfoTarget />
+    </div>
+  )
+}
+
+export default Information
